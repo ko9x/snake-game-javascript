de616f058f4e14539751249d14078b3cf2b32940
Sean McGillis <slcidevice@yahoo.com>
2022-09-22 16:47:14 -0600

    Got the food to only be placed where the snake is not present.

--- food.js
+++ food.js
@@ -1,28 +1,39 @@
 import { snakeBody, expandSnake } from "./snake.js";
 
 const food = { x: 6, y: 13 };
-const EXPANSION_RATE = 5;
+const EXPANSION_RATE = 20;
 const snakeTail = 0;
+let badNum = false;
 
 function getRandomSpot() {
-  let num = { x: 0, y: 0 };
+  let num = {};
+  console.log("here"); //@DEBUG
 
-// I needed to prevent 0 from occuring because it caused the food to be rendered but not eatable
+  // I needed to prevent 0 from occuring because it caused the food to be rendered but not eatable
   num.x = Math.floor(Math.random() * 20 + 1);
   num.y = Math.floor(Math.random() * 20 + 1);
 
-  snakeBody.forEach((segment) => {
-    if (segment.y === num.x) {
-      if (segment.x === num.y) {
-        console.log('SEGMENT', segment); //@DEBUG
-        console.log('NUM', num); //@DEBUG
-        return;
+  if (!badNum) {
+    snakeBody.forEach((segment) => {
+      if (segment.y === num.x) {
+        if (segment.x === num.y) {
+          num.bad = 1;
+          console.log("SEGMENT", segment); //@DEBUG
+          console.log("NUM", num); //@DEBUG
+        }
       }
-    }
-    food.x = num.x
-    food.y = num.y
-  });
-    
+      if (!badNum) {
+        // console.log("!badNum", num); //@DEBUG
+        food.x = num.x;
+        food.y = num.y;
+        food.bad = num.bad;
+      }
+      if (badNum) {
+        console.log("badNum", num); //@DEBUG
+        // getRandomSpot();
+      }
+    });
+  }
 }
 
 function onSnake(food) {
@@ -31,17 +42,24 @@ function onSnake(food) {
   }
 }
 
+getRandomSpot();
+
 export function update() {
   if (onSnake(food)) {
-    expandSnake(EXPANSION_RATE);
     getRandomSpot();
+    expandSnake(EXPANSION_RATE);
   }
 }
 
 export function draw(gameBoard) {
-  const foodElement = document.createElement("div");
-  foodElement.style.gridRowStart = food.x;
-  foodElement.style.gridColumnStart = food.y;
-  foodElement.classList.add("food");
-  gameBoard.appendChild(foodElement);
+  if (food.bad) {
+    console.log('hit the thing in the Draw!!!', ); //@DEBUG
+    getRandomSpot()
+  } else {
+    const foodElement = document.createElement("div");
+    foodElement.style.gridRowStart = food.x;
+    foodElement.style.gridColumnStart = food.y;
+    foodElement.classList.add("food");
+    gameBoard.appendChild(foodElement);
+  }
 }
