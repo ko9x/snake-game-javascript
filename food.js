0e38b814065c4abd1c9400ad706c4a100e164722
Sean McGillis <slcidevice@yahoo.com>
2022-09-21 22:58:11 -0600

    Trying to prevent the food from being placed on the snake but it still needs work.

--- food.js
+++ food.js
@@ -1,12 +1,28 @@
 import { snakeBody, expandSnake } from "./snake.js";
 
 const food = { x: 6, y: 13 };
-const EXPANSION_RATE = 3
+const EXPANSION_RATE = 5;
 const snakeTail = 0;
 
 function getRandomSpot() {
-  food.x = Math.floor(Math.random() * 21);
-  food.y = Math.floor(Math.random() * 21);
+  let num = { x: 0, y: 0 };
+
+// I needed to prevent 0 from occuring because it caused the food to be rendered but not eatable
+  num.x = Math.floor(Math.random() * 20 + 1);
+  num.y = Math.floor(Math.random() * 20 + 1);
+
+  snakeBody.forEach((segment) => {
+    if (segment.y === num.x) {
+      if (segment.x === num.y) {
+        console.log('SEGMENT', segment); //@DEBUG
+        console.log('NUM', num); //@DEBUG
+        return;
+      }
+    }
+    food.x = num.x
+    food.y = num.y
+  });
+    
 }
 
 function onSnake(food) {
@@ -18,6 +34,7 @@ function onSnake(food) {
 export function update() {
   if (onSnake(food)) {
     expandSnake(EXPANSION_RATE);
+    getRandomSpot();
   }
 }
 
